3d601d8128187b573bd381187c829cb732878477
simonesciacovelli <scias758@newschool.edu>
2018-11-01 18:51:15 -0400

    h

---
+++ assignment3/javascript.js
@@ -0,0 +1,120 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome Again " + user);
+    } else {
+       user = prompt("Please Enter Your Name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+$( ".page" ).mousemove(function( event ) {
+  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+  $( "span:first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+  $( "span:last" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
+});
+
+$( ".page" ).mousemove(function( event ) {
+var p = $( ".mydiv" );
+var position = p.position();
+$( ".pos1" ).text( "Left: " + position.left + ", Top: " + position.top );
+});
+
+$( ".page" ).mousemove(function( event ) {
+var p = $( ".mydiv2" );
+var position = p.position();
+$( ".pos2" ).text( "Left: " + position.left + ", Top: " + position.top );
+});
+
+$( ".page" ).mousemove(function( event ) {
+var p = $( ".mydiv3" );
+var position = p.position();
+$( ".pos3" ).text( "Left: " + position.left + ", Top: " + position.top );
+});
+
+$( ".page" ).mousemove(function( event ) {
+var p = $( ".mydiv4" );
+var position = p.position();
+$( ".pos4" ).text( "Left: " + position.left + ", Top: " + position.top );
+});
+
+$( ".page" ).mousemove(function( event ) {
+var p = $( ".mydiv5" );
+var position = p.position();
+$( ".pos5" ).text( "Left: " + position.left + ", Top: " + position.top );
+});
+
+$( function() {
+$( "#mydiv" ).draggable();});
+$("#mydiv").draggable({ containment: ".frame" });
+
+$( function() {
+$( "#mydiv2" ).draggable();});
+$("#mydiv2").draggable({ containment: ".frame" });
+
+$( function() {
+$( "#mydiv3" ).draggable();});
+$("#mydiv3").draggable({ containment: ".frame" });
+
+$( function() {
+$( "#mydiv4" ).draggable();});
+$("#mydiv4").draggable({ containment: ".frame" });
+
+$( function() {
+$( "#mydiv5" ).draggable();});
+$("#mydiv5").draggable({ containment: ".frame" });
+
+$( function() {
+$( "#mydiv6" ).draggable();});
+$("#mydiv6").draggable({ containment: ".frame" });
+
+
+$('.circle1').on('click', function(e) {
+  $(".page").toggleClass("page2");
+  $(".frame").toggleClass("frame2");
+  $(".circle1").toggleClass("circleblack");
+  $(".circle2").toggleClass("circleblack");
+  $(".circle3").toggleClass("circleblack");
+  $(".arrow").toggleClass("arrowhide");
+  $(".arrow2").toggleClass("arrowshow");
+  e.preventDefault();
+});
+
+$('.circle2').on('click', function(e) {
+  $(".mydiv").toggleClass("mydivalt");
+  e.preventDefault();
+});
+
+$('.frame').draggable({
+    axis: "y"
+});
+
+$( function() {
+$( ".frame" ).draggable();});
+$(".frame").draggable({ containment: ".page" });
