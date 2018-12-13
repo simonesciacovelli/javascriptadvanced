def01c812a89e578913eeeda3cb0a2ea16132405
simonesciacovelli <scias758@newschool.edu>
2018-12-13 18:04:05 -0500

    xc

--- assignment3/javascript.js
+++ assignment3/javascript.js
@@ -1,43 +1,11 @@
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome Again " + user);
-    } else {
-       user = prompt("Please Enter Your Name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
-}
+Cookies.set('mousemove', '', { expires: 7 });
 
 $( ".page" ).mousemove(function( event ) {
   var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
   var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
   $( "span:first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
-  $( "span:last" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
+  $( "span:last" ).text( "Cursor Coordinates : " + clientCoords );
 });
 
 $( ".page" ).mousemove(function( event ) {
